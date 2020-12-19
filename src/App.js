fe030eaf4f2d9b31347d0e9d413765f9518a32b9
modelearth <model@dreamstudio.com>
2020-12-19 00:31:22 -0500

    Change to 0.23

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Code for America</h1>
-        <h3>amplifyapp site</h3>
+        <h3>amplifyapp site 0.23</h3>
       </header>
     </div>
   );
